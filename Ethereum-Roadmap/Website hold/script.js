2af15969c79167f425ed329e4d892b8ba94aa1a8
Thornton James Keel <tj@keel.com>
2023-12-26 12:02:33 -0500

    style changes, add roadmap image

--- Ethereum-Roadmap/Website hold/script.js
+++ Ethereum-Roadmap/Website hold/script.js
@@ -15,23 +15,3 @@ function setProgressBar(id, progress) {
     progressBar.style.width = progress + '%';
   }
 }
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal
-var img = document.getElementById("roadmapImage");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
