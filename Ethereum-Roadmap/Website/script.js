bcafa1a4b6887a7bb3469b6ac7a35cd41dad6198
Thornton James Keel <tj@keel.com>
2024-01-28 14:41:05 -0500

    small fixes

--- Ethereum-Roadmap/Website/script.js
+++ Ethereum-Roadmap/Website/script.js
@@ -67,7 +67,7 @@ function updateCountdown() {
 setInterval(updateCountdown2, 1000);
 
 function updateCountdown2() {
-  const callDate = new Date('2025-03-31');
+  const callDate = new Date('2024-12-31');
   const now = new Date();
   const diff = callDate - now;
 
