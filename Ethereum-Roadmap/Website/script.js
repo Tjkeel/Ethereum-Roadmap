d7c8af3e5b33b2b6b9b794459f33eef68ce866e4
Thornton James Keel <tj@keel.com>
2024-02-08 12:12:15 -0600

    dencun mainnet date and time

--- Ethereum-Roadmap/Website/script.js
+++ Ethereum-Roadmap/Website/script.js
@@ -68,7 +68,7 @@ function updateCountdown3() {
 setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
-  const callDate = new Date('2024-03-13 13:55:35 UTC');
+  const callDate = new Date('2024-03-13');
   const now = new Date();
   const diff = callDate - now;
 
